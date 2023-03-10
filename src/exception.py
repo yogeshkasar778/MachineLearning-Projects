bfdd2f143bf59c0d37f4195eaa1e6eb06f6846de
Yogesh Kasar <kasary778@gmail.com>
2023-03-10 16:07:58 +0530

    EDA AND MODEL TRAINER

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -20,4 +20,7 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+
+
     
